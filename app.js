64eed6017e45f28cb72207bdcaf395d1d3f0e0a4
jackson0tr <mahmoudmohamedm82@gmail.com>
2024-06-25 00:53:17 +0200

    55

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 app.use(cors({
   // origin: ['http://localhost:3000'],
-  origin: ['https://dotte.vercel.app/'],
+  origin: ['https://dotte.vercel.app'],
   credentials: true
 }));
 
